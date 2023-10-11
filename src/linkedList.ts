be2c5999d1dfe0a8e597be19f49fe18ddde3a116
Mauricio <mauricioasoto@gmail.com>
2023-10-11 14:57:45 -0300

    add delete method to linkedList

--- src/linkedList.ts
+++ src/linkedList.ts
@@ -27,17 +27,41 @@ class LinkedList {
             current.next = newNode;
         }
     }
+    delete(data: String): void {
+        if (!this.head) {
+            return;
+        }
+        if (this.head.data === data) {
+            this.head = this.head.next;
+            return;
+        }
+        let current = this.head;
+        while (current.next) {
+            if (current.next.data === data) {
+                current.next = current.next.next;
+                return;
+            }
+            current = current.next;
+        }
+    }
 }
 
 const linkedList = new LinkedList();
 linkedList.append("Mauricio");
-linkedList.append("Rafael");
 linkedList.append("Foo");
+linkedList.append("Rafael");
 linkedList.append("Bar");
 console.log(linkedList)
-
+linkedList.delete("Foo")
+console.log(linkedList)
 /**
 npx ts-node src/linkedList.ts
+LinkedList {
+  head: ListNode {
+    data: 'Mauricio',
+    next: ListNode { data: 'Foo', next: [ListNode] }
+  }
+}
 LinkedList {
   head: ListNode {
     data: 'Mauricio',
