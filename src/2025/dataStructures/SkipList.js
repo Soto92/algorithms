32986df76fa4481ffe4b11e157059cb952050cc6
Mauricio <mauricioasoto@gmail.com>
2025-11-13 11:33:29 -0300

    Add changes in SkipList.js

---
+++ src/2025/dataStructures/SkipList.js
@@ -0,0 +1,60 @@
+class Node {
+  constructor(value, level) {
+    this.value = value;
+    this.forward = new Array(level + 1).fill(null);
+  }
+}
+
+class SkipList {
+  constructor(maxLevel = 16, p = 0.5) {
+    this.maxLevel = maxLevel;
+    this.p = p;
+    this.level = 0;
+    this.header = new Node(-1, this.maxLevel);
+  }
+
+  randomLevel() {
+    let lvl = 0;
+    while (Math.random() < this.p && lvl < this.maxLevel) {
+      lvl++;
+    }
+    return lvl;
+  }
+
+  insert(value) {
+    const update = new Array(this.maxLevel + 1).fill(null);
+    let current = this.header;
+
+    for (let i = this.level; i >= 0; i--) {
+      while (current.forward[i] && current.forward[i].value < value) {
+        current = current.forward[i];
+      }
+      update[i] = current;
+    }
+
+    const newLevel = this.randomLevel();
+    if (newLevel > this.level) {
+      for (let i = this.level + 1; i <= newLevel; i++) {
+        update[i] = this.header;
+      }
+      this.level = newLevel;
+    }
+
+    const newNode = new Node(value, newLevel);
+    for (let i = 0; i <= newLevel; i++) {
+      newNode.forward[i] = update[i].forward[i];
+      update[i].forward[i] = newNode;
+    }
+  }
+
+  search(value) {
+    let current = this.header;
+    for (let i = this.level; i >= 0; i--) {
+      while (current.forward[i] && current.forward[i].value < value) {
+        current = current.forward[i];
+      }
+    }
+    current = current.forward[0];
+    return current && current.value === value;
+  }
+}
