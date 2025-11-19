3940101cb085ca790a4a6fe5ac5f5325c1f396dd
Mauricio <mauricioasoto@gmail.com>
2025-11-19 08:06:19 -0300

    Add changes in CircularQueue.js

---
+++ src/2025/dataStructures/CircularQueue.js
@@ -0,0 +1,43 @@
+class CircularQueue {
+  constructor(capacity) {
+    this.items = new Array(capacity);
+    this.capacity = capacity;
+    this.currentLength = 0;
+    this.front = -1;
+    this.rear = -1;
+  }
+
+  isFull() {
+    return this.currentLength === this.capacity;
+  }
+
+  isEmpty() {
+    return this.currentLength === 0;
+  }
+
+  enqueue(item) {
+    if (!this.isFull()) {
+      this.rear = (this.rear + 1) % this.capacity;
+      this.items[this.rear] = item;
+      this.currentLength++;
+      if (this.front === -1) {
+        this.front = this.rear;
+      }
+    }
+  }
+
+  dequeue() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    const item = this.items[this.front];
+    this.items[this.front] = null;
+    this.front = (this.front + 1) % this.capacity;
+    this.currentLength--;
+    if (this.isEmpty()) {
+      this.front = -1;
+      this.rear = -1;
+    }
+    return item;
+  }
+}
