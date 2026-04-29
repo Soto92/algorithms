a525b6d9c2928e194ccbb63fab67c3b6bb0cb555
Mauricio <mauricioasoto@gmail.com>
2026-04-29 16:16:27 -0300

    move files

--- src/theOnlyone.ts
+++
@@ -1,9 +0,0 @@
-const solution = (list: number[]) => {
-    let sum = 0
-    list.forEach(element => {
-        sum = sum ^ element
-    });
-    return sum
-}
-
-console.log(solution([2, 2, 7, 4, 4]))
